bd82f8f98c5396cde9eda2945ecffa64eaae5e5b
Oscar Otero <oom@oscarotero.com>
2020-06-16 21:05:02 +0200

    axustes

--- site/js/dialog.js
+++ site/js/dialog.js
@@ -1,10 +1,17 @@
 import dialogPolyfill from '../vendors/dialog-polyfill/dialog-polyfill.esm.js';
 
-document.querySelectorAll('dialog').forEach((dialog) => dialogPolyfill.registerDialog(dialog));
+const scrollBarWidth = getScrollbarWidth()
+
+document.querySelectorAll('dialog').forEach((dialog) => {
+    dialogPolyfill.registerDialog(dialog);
+
+    dialog.addEventListener('close', releaseScroll);
+});
+
 document.querySelectorAll('.js-openDialog').forEach((element) =>
     element.addEventListener('click', () => {
         const dialog = element.nextElementSibling;
-
+        blockScroll();
         dialog.showModal();
     })
 );
@@ -16,3 +23,34 @@ document.querySelectorAll('.js-closeDialog').forEach((element) =>
         dialog.close();
     })
 );
+
+
+function getScrollbarWidth() {
+    const outer = document.createElement('div')
+    outer.style.visibility = 'hidden'
+    outer.style.overflow = 'scroll'
+    const inner = document.createElement('div')
+    outer.appendChild(inner)
+  
+    document.body.appendChild(outer)
+    const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth)
+    outer.parentNode.removeChild(outer)
+  
+    return scrollbarWidth
+  }
+  
+  function blockScroll() {
+      const element = document.scrollingElement;
+
+    if (element.scrollHeight > element.clientHeight) {
+      element.style.overflow = 'hidden'
+      element.style.paddingRight = `${scrollBarWidth}px`
+    }
+  }
+  
+  function releaseScroll() {
+    const element = document.scrollingElement;
+
+    element.style.overflow = null
+    element.style.paddingRight = null
+  }
