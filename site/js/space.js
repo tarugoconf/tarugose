d14ee0b5dd0116d68432cae070903ad24aa0f9c7
Oscar Otero <oom@oscarotero.com>
2020-06-27 16:48:40 +0200

    bug en firefox android

--- site/js/space.js
+++ site/js/space.js
@@ -2,7 +2,7 @@ import Generator from '../vendors/@oom/particles/generator.js';
 import Canvas from '../vendors/@oom/particles/canvas.js';
 
 const generator = new Generator()
-    .color(['#ffffff', '#ffff66'])
+    .color('#ffffff')
     .opacity({ min: 0.2, max: 1})
     .size({ min: 0.1, max: 2 })
     .position({ 
@@ -15,7 +15,8 @@ const canvas = new Canvas(
     document.querySelector('.js-space'),
     generator,
     {
-        number: 100
+        number: 150,
+        resize: 'ResizeObserver' in window ? 'repaint' : ''
     }
 );
 
