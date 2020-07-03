ff2d6bfb059da941e1622b2a1646eec491a6c6ab
David Bonilla <david@bonillaware.com>
2020-07-03 21:32:39 +0200

    Añadido código de conducta

--- site/_data/footer.js
+++ site/_data/footer.js
@@ -2,12 +2,16 @@ module.exports = {
     copyright: '© 2020 Bonillaware SLU',
     links: [
         {
-            text: 'Aviso legal',
+            text: 'Aviso Legal',
             url: '/aviso-legal',
         },
         {
-            text: 'Política de privacidad',
+            text: 'Política de Privacidad',
             url: '/politica-de-privacidad',
+        },
+        {
+            text: 'Código de Conducta',
+            url: '/codigo-de-conducta',
         },
     ],
 };
